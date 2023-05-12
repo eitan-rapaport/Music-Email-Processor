96b7ad784cc32ac93093c494705bd6125d460ed8
Eitan Rapaport <erlior@gmail.com>
2023-05-12 13:31:14 +0300

    Made the main method smailler

--- DownloadMomsEmails_normalize_parallel.py
+++ DownloadMomsEmails_normalize_parallel.py
@@ -10,6 +10,7 @@
 # TODO: filter lists out
 
 from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
 from pydub.effects import normalize
 from pydub.effects import compress_dynamic_range
 import re
@@ -24,6 +25,10 @@ import traceback
 import sys
 import cyrtranslit
 import glob
+import logging
+
+
+
 
 # Vars:
 SILENCE_AT_BEGINNING_AND_END_MS = 3000
@@ -34,6 +39,8 @@ parser.add_argument('--download-video', action='store_true', default=False,
                     help="Download the video")
 parser.add_argument('--keep-original', action='store_true', default=False,
                     help="Keep the not normalized mp3 file")
+parser.add_argument('--no-edit', action='store_true', default=False,
+                    help="disable adding silence, compressing and normalizing")
 
 args = parser.parse_args()
 
@@ -60,21 +67,22 @@ def create_folder(path):
 
     if os.path.exists(path):
         print('Folder exists')
-        os.chdir(path)
+        
     else:
         os.makedirs(path)
         print('Created folder ' + folder)
-        os.chdir(path)
 
+    os.chdir(path)
 
 def download(link):
     # Download audio
+    logging.info(f"Downloading {link}")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
     except Exception as e:
-        print("Exception while downloading MP3!")
-        print(e)
+        logging.error(f"Exception while downloading MP3 for {link}", exc_info=e)
+        logging.error(e)
         exceptions['MP3'].append(link)
 
     # Download Video
@@ -83,8 +91,8 @@ def download(link):
             with yt_dlp.YoutubeDL(ydl_video) as ydl:
                 ydl.download([link])
         except Exception as e:
-            print("Exception while downloading Video!")
-            print(e)
+            logging.error(f"Exception while downloading Video for {link}", exc_info=e)
+            logging.error(e)
             exceptions['Video'].append(link)
 
 
@@ -103,33 +111,34 @@ def match_target_amplitude(sound, target_dBFS):
 
 
 def print_exceptions(exceptions):
-    print("### MP3 Exceptions ###")
-    for i in exceptions['MP3']:
-        print(i)
+    if len(exceptions['MP3']) > 0:
+        logging.error("### MP3 Exceptions ###")
+        for i in exceptions['MP3']:
+            logging.error(i)
 
-    print("### Video Exceptions ###")
-    for i in exceptions['Video']:
-        print(i)
+    if len(exceptions['Video']) > 0:
+        logging.error("### Video Exceptions ###")
+        for i in exceptions['Video']:
+            logging.error(i)
 
 def print_found_urls(urls):
-    print("1. Found the following URIs, {0} in total:".format(len(urls)))
+    logging.info("1. Found the following URIs, {0} in total:".format(len(urls)))
     for i in urls:
-        print(i)
-
-    print()
+        logging.info(i)
 
 def download_all_uris(urls):
-    print("2. Downloading URLs")
+    logging.info("2. Downloading URLs")
     with Pool() as pool:
         try:
             pool.map(download, urls)
         except Exception as e:
-            print("Encountered exception: ", e, "for file: ", urls)
+            logging.error("Encountered exception: ", e, "for file: ", urls)
             traceback.print_exc(file=sys.stdout)
-    print("2. Finished downloading")
+    logging.info("2. Finished downloading")
         
 def normalize_all_filenames(files):
-    print("3. Normalizing all file names")
+    files = get_file_list()
+    logging.info("3. Normalizing all file names")
     for file in files:
         normalize_filename(file)
 
@@ -137,41 +146,43 @@ def normalize_filename(file):
     if not file.endswith('.mp3'):
         return
 
-    print("3.1 Normalizing {0}".format(file))
+    logging.info("3.1 Normalizing {0}".format(file))
     new_name = re.sub('\W+', ' ', cyrtranslit.to_latin(file, "ru"))
     new_name = re.sub(' mp3', ".mp3", new_name)
     os.replace(file, new_name)
-    print("3.1 Replaced. Old file name: {0}, New file name: {1}".format(file, new_name))
+    logging.info("3.1 Replaced. Old file name: {0}, New file name: {1}".format(file, new_name))
 
-def compress_all_files(files):
-    print(f"4. Compressing all files")
+def compress_all_files():
+    files = get_file_list()
+    logging.info(f"4. Compressing all files")
     with Pool() as pool:
         pool.map(compress_file, files)
-    print("4. Finished compressing")
+    logging.info("4. Finished compressing")
 
 
 def compress_file(file):
     if not file.endswith('.mp3'):
         pass
     else:
-        print(f'4.1 Compressing: -- {file}')
+        logging.info(f'4.1 Compressing: -- {file}')
         raw_sound = AudioSegment.from_file(file)
         compressed_sound = compress_dynamic_range(raw_sound, -20, 6)
         new_file = re.sub("\.mp3", "_C.mp3", file)
         compressed_sound.export(new_file , format='mp3')
 
         if args.keep_original == False:
-            print("Deleting " + file)
+            logging.info("Deleting " + file)
             os.remove(file)
 
-        print(f"4.1 Compressed {new_file}")
+        logging.info(f"4.1 Compressed {new_file}")
 
 
-def normalize_all_audio_files(files):
-    print("5. Normalizing files...")
+def normalize_all_audio_files():
+    files = get_file_list()
+    logging.info("5. Normalizing files...")
     with Pool() as pool:
         pool.map(normalize_audio_file, files)
-    print("5. Finished normalizing")
+    logging.info("5. Finished normalizing")
 
 
 def normalize_audio_file(file):
@@ -179,27 +190,28 @@ def normalize_audio_file(file):
         pass
 
     else:
-        print(f"5.1 Normalizing {file}")
+        logging.info(f"5.1 Normalizing {file}")
         raw_sound = AudioSegment.from_file(file)
         normalized_file = normalize(raw_sound, -1)
         new_file = re.sub("\.mp3", "N.mp3", file)
         normalized_file.export(new_file, format='mp3')
 
         if args.keep_original == False:
-            print("Deleting " + file)
+            logging.info("Deleting " + file)
             os.remove(file)   
-        print(f"5.1 Normalized {new_file}")     
+        logging.info(f"5.1 Normalized {new_file}")     
 
 
-def add_ms_of_silence_to_all_files(files):
-    print("6. Add silence to files")
+def add_ms_of_silence_to_all_files():
+    files = get_file_list()
+    logging.info("6. Add silence to files")
     with Pool() as pool:
         pool.map(add_silence_to_file, files)
-    print("6. Finished adding silence ")
+    logging.info("6. Finished adding silence ")
 
 
 def add_silence_to_file(file):
-    print(f"6.1 Adding silence to file {file}")
+    logging.info(f"6.1 Adding silence to file {file}")
     raw_sound = AudioSegment.from_file(file)
     sample = raw_sound[:SILENCE_AT_BEGINNING_AND_END_MS]
     sample = sample - 70
@@ -208,10 +220,10 @@ def add_silence_to_file(file):
     output.export(new_file, format='mp3')
 
     if args.keep_original == False:
-        print("Deleting " + file)
+        logging.info("Deleting " + file)
         os.remove(file)
 
-    print(f"6.1 Added silence to {new_file}")
+    logging.info(f"6.1 Added silence to {new_file}")
 
 
 def read_email():
@@ -245,27 +257,44 @@ def find_urls_in_email(email_content):
 
 
 def get_file_list():
-    print("Getting file list")
+    logging.info("Getting file list")
     files = glob.glob("*.mp3")
-    print(files)
+    logging.info(files)
     return files
+
+def configure_logging():
+    logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s,%(funcName)s,%(levelname)s,%(message)s',
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+        ]
+    )
+
+def remove_clapping(file_list):
+    audio = AudioSegment.from_wav(file_list[0])
+    min_silence_len = 1000  # minimum silence length in milliseconds
+    silence_thresh = -50  # silence threshold in dBFS
+    non_silent_parts = detect_nonsilent(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
+    audio_no_claps = AudioSegment.silent(duration=0)
+    for start, end in non_silent_parts:
+        audio_no_claps += audio[start:end]
+
 
 
 def main():
     create_folder(path)
+    configure_logging()
     email_content = read_email()
     urls = find_urls_in_email(email_content)
     print_found_urls(urls)
     download_all_uris(urls)
-    file_list = get_file_list()
-    normalize_all_filenames(file_list)
-    file_list = get_file_list()
-    compress_all_files(file_list)
-    file_list = get_file_list()
-    normalize_all_audio_files(file_list)
-    file_list = get_file_list()
-    add_ms_of_silence_to_all_files(file_list)
+    compress_all_files()
+    normalize_all_audio_files()
+    add_ms_of_silence_to_all_files()
     print_exceptions(exceptions)
+    #remove_clapping(file_list)
 
 
 if __name__ == '__main__':
